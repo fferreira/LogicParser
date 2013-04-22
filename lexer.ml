4b8440922cd1d347f159cfa0b5c203e889c4cb2b
Francisco Ferreira <fferre8@cs.mcgill.ca>
2013-04-22 17:24:24 -0400

    improving position handling

--- lexer.ml
+++ lexer.ml
@@ -14,21 +14,35 @@ let regexp upper = ['A'-'Z']
 let regexp var = lower (lower | upper)*
 let regexp prop = upper (lower | upper)*
 
-let rec main_scanner nlines = lexer
-| wsp | tab			-> main_scanner nlines lexbuf         (* ignore whitespace *)
-| nl				-> main_scanner (succ nlines) lexbuf   (* ignores new lines *)
+(* Managing source code positions *)
 
-| prop			        -> nlines, PROP (Ulexing.utf8_lexeme lexbuf)
-| var			        -> nlines, VAR (Ulexing.utf8_lexeme lexbuf)
-| 0x2227 (* ∧ *)		-> nlines, AND
-| 0x2228 (* ∨ *)		-> nlines, OR
-| 0x2200 (* ∀ *)		-> nlines, FORALL
-| 0x2203 (* ∃ *)		-> nlines, EXISTS
-| 0x2283 (* ⊃ *)                -> nlines, IMP 
-| 0x00AC (* ¬ *)                -> nlines, NOT
-| '.'				-> nlines, DOT
-| '('				-> nlines, LPAREN
-| ')'				-> nlines, RPAREN
-| ';'				-> nlines, SEMICOLON
-| eof				-> nlines, EOF
-| _                             -> nlines, WHAT (Ulexing.utf8_lexeme lexbuf)
+let initial_pos file_name = { Lexing.pos_fname = file_name ; 
+			      Lexing.pos_lnum = 1 ; 
+			      Lexing.pos_bol = 0 ; 
+			      Lexing.pos_cnum = 0 }
+let add_line pos = { pos with 
+		     Lexing.pos_lnum = pos.Lexing.pos_lnum +1 ; 
+		     Lexing.pos_bol = pos.Lexing.pos_bol + pos.Lexing.pos_cnum ; 
+		     Lexing.pos_cnum = 0} 
+let add_word pos length = { pos with Lexing.pos_cnum = pos.Lexing.pos_cnum + length }
+
+(* The lexer *)
+
+let rec main_scanner pos = lexer
+| wsp | tab			-> main_scanner (add_word pos 1) lexbuf (* ignore whitespace *)
+| nl				-> main_scanner (add_line pos) lexbuf   (* ignores new lines *)
+
+| prop			        -> add_word pos (Ulexing.lexeme_length lexbuf), PROP (Ulexing.utf8_lexeme lexbuf)
+| var			        -> add_word pos (Ulexing.lexeme_length lexbuf), VAR (Ulexing.utf8_lexeme lexbuf)
+| 0x2227 (* ∧ *)		-> add_word pos (Ulexing.lexeme_length lexbuf), AND
+| 0x2228 (* ∨ *)		-> add_word pos (Ulexing.lexeme_length lexbuf), OR
+| 0x2200 (* ∀ *)		-> add_word pos (Ulexing.lexeme_length lexbuf), FORALL
+| 0x2203 (* ∃ *)		-> add_word pos (Ulexing.lexeme_length lexbuf), EXISTS
+| 0x2283 (* ⊃ *)                -> add_word pos (Ulexing.lexeme_length lexbuf), IMP 
+| 0x00AC (* ¬ *)                -> add_word pos (Ulexing.lexeme_length lexbuf), NOT
+| '.'				-> add_word pos (Ulexing.lexeme_length lexbuf), DOT
+| '('				-> add_word pos (Ulexing.lexeme_length lexbuf), LPAREN
+| ')'				-> add_word pos (Ulexing.lexeme_length lexbuf), RPAREN
+| ';'				-> add_word pos (Ulexing.lexeme_length lexbuf), SEMICOLON
+| eof				-> add_word pos (Ulexing.lexeme_length lexbuf), EOF
+| _                             -> add_word pos (Ulexing.lexeme_length lexbuf), WHAT (Ulexing.utf8_lexeme lexbuf)
--- lparser.ml
+++ lparser.ml
@@ -4,15 +4,12 @@ exception Syntax_error of Lexing.position
 let file_name = "pepe.pipi"
 
 let parse menhir_parser lexbuf =
-  (* TODO: columns are not being updated, Lexing module should not be used? *)
-  let position = ref
-      Lexing.({ pos_fname = file_name; pos_lnum = 1; pos_bol = 0; pos_cnum = 0 }) in
+  let position = ref (Lexer.initial_pos file_name) in
   let lexer () =
     let ante_position = !position in
-    let nlines, token = Lexer.main_scanner 0 lexbuf in
-    let () = position := Lexing.({!position with pos_lnum = !position.pos_lnum + nlines;}) in
-    let post_position = !position
-    in (token, ante_position, post_position) in
+    let post_position, token = Lexer.main_scanner !position lexbuf in
+    let () = position := post_position in (* Lexing.({!position with pos_lnum = !position.pos_lnum + nlines;}) in *)
+    (token, ante_position, post_position) in
   let revised_parser = MenhirLib.Convert.Simplified.traditional2revised menhir_parser
   in try
        revised_parser lexer
@@ -32,5 +29,5 @@ let () =
     let c = parse Parser.props lexbuf in
     print_string (List.fold_right (fun x -> fun r -> (Syntax.to_string x) ^ "\n" ^ r) c "done\n")
   with
-  | Syntax_error pos -> Printf.printf "Syntax error in line %d\n" pos.Lexing.pos_lnum
+  | Syntax_error pos -> Printf.printf "Syntax error in line %d, col %d\n" pos.Lexing.pos_lnum pos.Lexing.pos_cnum
 
